2bd0197e05294d270c6da2094e55ae3854d9b73e
funasshi <atsuyoshi.45568997@gmail.com>
2020-10-25 11:00:59 +0900

    生成画像の登録

--- do.py
+++ do.py
@@ -8,9 +8,23 @@ generator = Generator()
 model_path = 'trained_generator.pth'
 generator.load_state_dict(torch.load(model_path,map_location=torch.device('cpu')))
 generator.eval()
-fake=generator(torch.rand((1,100)))
-fake=fake.reshape(28,28)
-fake=fake.detach().numpy()
 
-plt.imshow((fake+1)/2,cmap="gray")
+
+fig=plt.figure()
+for i in range(1,21):
+    ax=fig.add_subplot(4,5,i)
+    fake=generator(torch.rand((1,100)))
+    fake=fake.reshape(28,28)
+    fake=fake.detach().numpy()
+    ax.tick_params(labelbottom=False,
+                   labelleft=False,
+                   labelright=False,
+                   labeltop=False)
+    ax.tick_params(bottom=False,
+                   left=False,
+                   right=False,
+                   top=False)
+    ax.imshow((fake+1)/2,cmap="gray")
+
+plt.savefig("generated_img.png")
 plt.show()
